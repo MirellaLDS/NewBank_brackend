c23192b9e09924b9a7c35fbc603a908a582dcf91
Mirella Lins <mirella.lds@gmail.com>
2020-08-19 23:55:25 -0300

    channel

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGODB, {
 
 client.on('ready', () => {
     console.log('I am ready!');
-    const chanel = client.channels.cache.get("geral");
+    const chanel = client.channels.cache.get(745396266044883006);
     chanel.send("i'm here!", {
         files: [
             "./images/ry5enok9.bmp"
